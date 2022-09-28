f321152c8a021cb8b3cd73ef60a1d5133c1b97cf
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-28 16:05:24 +0530

    Add files via upload
    adding n numbers using lambda expression

---
+++ lamdasum28/lamdasum28/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace lamdasum28
+{
+    //public delegate void cal(int a);
+    internal class Program
+    {
+        public delegate int cal(int a);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            cal add = (p) =>
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    sum = sum + i;
+                   
+                }
+                return sum;
+            };
+
+            cal obj = new cal(add);
+            Console.WriteLine("the sum of the numbers is:"+obj.Invoke(n));
+
+        }
+    }
+}
