3f708891b5678bd44d459376b54263120c1a1cdf
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-28 15:47:14 +0530

    Add files via upload
    anonymous function

---
+++ anonymousinterest/anonymousinterest/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace anonymousinterest
+{
+    public delegate void interest(int p,double r,int t);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            interest obj = delegate (int p,double r,int t)
+            {
+                Console.WriteLine("The simple interest that needs to be paid is :"+p * r * t);
+
+            };
+            Console.WriteLine("enter the principle amt");
+            int p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the rate of interest");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the number of years");
+            int y=Convert.ToInt32(Console.ReadLine());
+            obj.Invoke(p, r, y);
+
+        }
+    }
+}
